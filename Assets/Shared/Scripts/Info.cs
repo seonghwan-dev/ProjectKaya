bd15e06ab7b595121274708d649e9a116a031ea8
seonghwan-dev <Seonghwan@pm.me>
2022-05-08 23:18:22 +0900

    refactor: 공유 리소스 에셋 폴더 내 패키지로 관리

---
+++ Assets/Shared/Scripts/Info.cs
@@ -0,0 +1,23 @@
+﻿namespace Kaya
+{
+	public static class Info
+	{
+		public class Version
+		{
+			public readonly int Major;
+			public readonly int Minor;
+			public readonly int Patch;
+			public readonly int Revision;
+
+			public Version(int major, int minor, int patch, int revision)
+			{
+				this.Major = major;
+				this.Minor = minor;
+				this.Patch = patch;
+				this.Revision = revision;
+			}
+		}
+
+		public static readonly Version AssetVersion = new Version(0, 0, 1, 0);
+	}
+}
